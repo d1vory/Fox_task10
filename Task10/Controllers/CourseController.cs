65e16a31cfaf0b3ccb99ef12cc7342e0966d6fac
Olexandr <dubchakob@gmail.com>
2024-05-14 22:27:39 +0300

    edit/create

--- Task10/Controllers/CourseController.cs
+++ Task10/Controllers/CourseController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Task10.Data;
+using Task10.Models;
 
 namespace Task10.Controllers;
 
@@ -24,16 +25,38 @@ public class CourseController: Controller
         return View();
     }
 
-    // public async Task<IActionResult> Edit(int? id)
-    // {
-    //     if (!id.HasValue)
-    //     {
-    //         return NotFound();
-    //     }
-    //
-    //     var course = await _db.FindAsync(id.Value);
-    //
-    // }
+    [HttpPost]
+    public async Task<IActionResult> Create(Course course)
+    {
+        await _db.Courses.AddAsync(course);
+        await _db.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
     
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (!id.HasValue)
+        {
+            return NotFound();
+        }
+    
+        var course = await _db.Courses.FindAsync(id.Value);
+        if (course == null)
+        {
+            return NotFound();
+        }
+
+        return View(course);
+    }
+    
+    
+    [HttpPost]
+    public async Task<IActionResult> Edit(Course course)
+    {
+        _db.Courses.Update(course);
+        await _db.SaveChangesAsync();
+        return RedirectToAction("Index");
+    }
+
     
 }
--- Task10/Controllers/HomeController.cs
+++ Task10/Controllers/HomeController.cs
@@ -24,13 +24,6 @@ public class HomeController : Controller
         return View();
     }
     
-    public IActionResult Courses()
-    {
-        var db = new ApplicationContext();
-        var courses = db.Courses.ToList();
-        return View(courses);
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
